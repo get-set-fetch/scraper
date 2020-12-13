f520193a06826beb41b952eb0ea84876af4decb2
Andrei Sabau <andrei@asabau.com>
2020-12-13 15:43:59 +0200

    allow ts-node compilation of other get-set-fetch node-modules, #21

--- test/utils/ts-node-config.js
+++ test/utils/ts-node-config.js
@@ -3,6 +3,5 @@ require('ts-node').register({
   files: true,
   pretty: true,
   'no-cache': true,
+  ignore: [ /node_modules\/(?!get-set-fetch-test-utils)/ ],
 });
-
-console.log('done register ts-node');
