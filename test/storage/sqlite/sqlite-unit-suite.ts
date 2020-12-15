43b0e6d1cf9a8913950ec800ed071daccc582609
Andrei Sabau <andrei@asabau.com>
2020-12-15 21:54:49 +0200

    split sqlite tests into unit and integration suites, #2

---
+++ test/storage/sqlite/sqlite-unit-suite.ts
@@ -0,0 +1,7 @@
+import Storage from '../../../src/storage/base/Storage';
+import KnexStorage from '../../../src/storage/knex/KnexStorage';
+import unitSuite from '../../unit/storage/unit-suite';
+import * as conn from './sqlite-conn.json';
+
+const storage:Storage = new KnexStorage(conn);
+unitSuite(storage);
