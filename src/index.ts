dbcd5b39d7a308b48e899bec64c03824e7df1830
Andrei Sabau <andrei@asabau.com>
2020-12-19 10:07:13 +0200

    package export update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import KnexStorage from './storage/knex/KnexStorage';
+import PuppeteerClient from './browserclient/PuppeteerClient';
 import Scraper from './scraper/Scraper';
 import { getLogger, setLogger } from './logger/Logger';
 
 export {
+  KnexStorage,
+  PuppeteerClient,
   Scraper,
   getLogger, setLogger,
 };
