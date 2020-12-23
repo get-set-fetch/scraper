35716a373fbcbed2f7456b00901f5dd3a314374b
Andrei Sabau <andrei@asabau.com>
2020-12-23 19:23:50 +0200

    simplified scenario names

--- src/scenarios/scenarios.ts
+++ src/scenarios/scenarios.ts
@@ -1,18 +1,49 @@
 import { IPluginOpts } from '../plugins/Plugin';
-import ScrapeStaticContent from './ScrapeStaticContent';
+import StaticContentScenario from './StaticContentScenario';
 
 interface IScenario {
   defaultPluginOpts:IPluginOpts[];
 }
 
-const scenarios: {[key: string] : IScenario} = {
-  ScrapeStaticContent,
+export type Scenarios = {
+  [key: string] : IScenario
+}
+
+const scenarios: Scenarios = {
+  'static-content': StaticContentScenario,
 };
 
-const mergePluginOpts = (defaultOpts: IPluginOpts[], customOpts: IPluginOpts[]):IPluginOpts[] => defaultOpts.map(defaultOpt => {
-  const customOpt = customOpts.find(opt => opt.name === defaultOpt.name) || {};
-  return { ...defaultOpt, ...customOpt };
-});
+const mergePluginOpts = (defaultOpts: IPluginOpts[], customOpts: IPluginOpts[]):IPluginOpts[] => {
+  const mergeOpts:IPluginOpts[] = [ ...defaultOpts ];
+  customOpts.forEach(pluginCustomOpts => {
+    if (pluginCustomOpts.before) {
+      const idx = mergeOpts.findIndex(mergePluginOpts => mergePluginOpts.name === pluginCustomOpts.before);
+      if (idx === -1) throw new Error(`could not find plugin ${pluginCustomOpts.before} as before anchor`);
+      mergeOpts.splice(idx, 0, pluginCustomOpts);
+      return;
+    }
+
+    if (pluginCustomOpts.replace) {
+      const idx = mergeOpts.findIndex(mergePluginOpts => mergePluginOpts.name === pluginCustomOpts.replace);
+      if (idx === -1) throw new Error(`could not find plugin ${pluginCustomOpts.before} as replace anchor`);
+      mergeOpts[idx] = pluginCustomOpts;
+      return;
+    }
+
+    if (pluginCustomOpts.after) {
+      const idx = mergeOpts.findIndex(mergePluginOpts => mergePluginOpts.name === pluginCustomOpts.after);
+      if (idx === -1) throw new Error(`could not find plugin ${pluginCustomOpts.before} as after anchor`);
+      mergeOpts.splice(idx + 1, 0, pluginCustomOpts);
+      return;
+    }
+
+    const idx = mergeOpts.findIndex(mergePluginOpts => mergePluginOpts.name === pluginCustomOpts.name);
+    if (idx === -1) throw new Error(`could not find plugin ${pluginCustomOpts.name} as merge anchor`);
+    mergeOpts[idx] = { ...mergeOpts[idx], ...pluginCustomOpts };
+  });
+
+  return mergeOpts;
+};
 
 export {
   IScenario,
