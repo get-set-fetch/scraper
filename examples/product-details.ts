9b796d72fe86be0852e23d602722a98608d26ebe
Andrei Sabau <andrei@asabau.com>
2021-01-03 19:13:37 +0200

    examples update

---
+++ examples/product-details.ts
@@ -0,0 +1,78 @@
+/* eslint-disable object-curly-newline */
+import { KnexStorage, PuppeteerClient, Scraper, setLogger } from '../src/index';
+
+setLogger({ level: 'info' });
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './examples/data/books.sqlite',
+  },
+};
+const storage = new KnexStorage(knexConfig);
+const client = new PuppeteerClient({ args: [
+  '--disable-gpu',
+  '--disable-dev-shm-usage',
+  '--disable-setuid-sandbox',
+  '--no-first-run',
+  '--no-sandbox',
+  '--no-zygote',
+  '--single-process',
+] });
+const scraper = new Scraper(storage, client);
+
+const scrapeOptsHash = 'eLt7R4n7pVRdS8MwFP0rob4omJS2PglTxp4Gg+3JFydy24YmLE1G0nbsxd/uTeOwnR0OpU/pvTnnfpyTc9XgurSSuQV7ZMZWMbSNMNbF61X2kKbJPF46gOJ97mRtuuc9VHyW/ElfF7r5Kj8dlz+liu/lZr9L5MZxsIVAOvSKIx+EYelSg58zeSLAFsIYxzfYz6NuBC2EVOVtehf5IsZQImO5MbtecHjRBsiJREAHdtyuMBkpZF2Fsx/7j86us9nAWyJhB2N3tC8Dk08OPJ2njXc/xkgZL6UfAc2PSmpsZ4AUNn8lVKtwEFBy66jfv8OGlWTQVdTijHXlf7g96FdUfb1BwNk2CpEXUC3fRm8D5hAgnY/8h3+Kk3eSHxam1c0ZZx8ghY9cfrfw+wQSXx87';
+const scrapeOpts = {
+  url: 'https://openlibrary.org/authors/OL34221A/Isaac_Asimov?page=1',
+  scenario: 'static-content',
+  pluginOpts: [
+    {
+      name: 'SelectResourcePlugin',
+      delay: 2000,
+    },
+    {
+      name: 'ExtractUrlsPlugin',
+      maxDepth: 3,
+      selectorPairs: [
+        {
+          urlSelector: '#searchResults ~ .pagination > a.ChoosePage:nth-child(2)',
+        },
+        {
+          urlSelector: 'h3.booktitle a.results',
+        },
+        {
+          urlSelector: 'a.coverLook > img.cover',
+        },
+      ],
+    },
+    {
+      name: 'ExtractHtmlContentPlugin',
+      selectorPairs: [
+        {
+          selector: 'h1.work-title',
+          label: 'title',
+        },
+        {
+          selector: 'h2.edition-byline a',
+          label: 'author',
+        },
+        {
+          selector: 'ul.readers-stats > li.avg-ratings > span[itemProp="ratingValue"]',
+          label: 'rating value',
+        },
+        {
+          selector: 'ul.readers-stats > li > span[itemProp="reviewCount"]',
+          label: 'review count',
+        },
+      ],
+    },
+  ],
+};
+
+(async () => {
+  await scraper.scrape(scrapeOpts);
+  await scraper.export('./examples/data/books.csv', { type: 'csv' });
+  await scraper.export('./examples/data/book-covers.zip', { type: 'zip' });
+  await storage.close();
+})();
---
+++ examples/table-scraping.ts
@@ -0,0 +1,58 @@
+/* eslint-disable object-curly-newline */
+import { encode, KnexStorage, PuppeteerClient, Scraper, setLogger } from '../src/index';
+
+setLogger({ level: 'info' });
+
+const knexConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: './examples/data/languages.sqlite',
+  },
+};
+
+const storage = new KnexStorage(knexConfig);
+const client = new PuppeteerClient({ args: [
+  '--disable-gpu',
+  '--disable-dev-shm-usage',
+  '--disable-setuid-sandbox',
+  '--no-first-run',
+  '--no-sandbox',
+  '--no-zygote',
+  '--single-process',
+] });
+const scraper = new Scraper(storage, client);
+
+const scrapeOptsHash = 'eLt7R4n7pZNBCsIwEEWvkmWLmoruuvAE3iFMzTQNTdLQpBVv79QSrEpBKCEw+Yv5zPyXb2rQ8btutUepgXe9KqZXcdUhiq4WBpwaQGEQ1UO4wVbYT7IjokYUwSO0SBFt4O0j+Hd+x19E/iNzgSOFapBbjCAhAtsxT3cWpyFfFYuydLE53BptZHbK2YVBWVOzOEvkkVhPu1ilfL/R/Zwv3NJuWWaJTIIjX/9ddJ43QKbJ';
+
+const scrapeOpts = {
+  url: 'https://en.wikipedia.org/wiki/List_of_languages_by_number_of_native_speakers',
+  scenario: 'static-content',
+  pluginOpts: [
+    {
+      name: 'ExtractUrlsPlugin',
+      maxDepth: 0,
+    },
+    {
+      name: 'ExtractHtmlContentPlugin',
+      selectorPairs: [
+        {
+          selector: 'table.metadata + p + table.wikitable td:nth-child(2) > a:first-child',
+          label: 'language',
+        },
+        {
+          selector: 'table.metadata + p + table.wikitable td:nth-child(3)',
+          label: 'speakers (milions)',
+        },
+      ],
+    },
+  ],
+};
+
+(async () => {
+  await scraper.scrape(scrapeOptsHash);
+  await scraper.export('./examples/data/languages.csv', { type: 'csv' });
+  await storage.close();
+
+  encode(scrapeOpts);
+})();
