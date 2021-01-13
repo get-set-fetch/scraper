1e232c89208d33645f8fae8cb2561d683c5ee189
Andrei Sabau <andrei@asabau.com>
2021-01-13 21:00:19 +0200

    export updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import KnexStorage from './storage/knex/KnexStorage';
+import Project from './storage/base/Project';
+import Resource from './storage/base/Resource';
 import PuppeteerClient from './browserclient/PuppeteerClient';
-import Scraper from './scraper/Scraper';
+import Plugin, { PluginOpts } from './plugins/Plugin';
+import PluginStore, { StoreEntry } from './pluginstore/PluginStore';
+import Scraper, { ScrapeDefinition } from './scraper/Scraper';
 import { getLogger, setLogger } from './logger/Logger';
 import { encode, decode } from './confighash/config-hash';
 import CsvExporter from './export/CsvExporter';
@@ -8,9 +12,11 @@ import ZipExporter from './export/ZipExporter';
 import { scenarios, mergePluginOpts } from './scenarios/scenarios';
 
 export {
-  KnexStorage,
+  KnexStorage, Project, Resource,
   PuppeteerClient,
-  Scraper,
+  PluginStore, StoreEntry,
+  Plugin, PluginOpts,
+  Scraper, ScrapeDefinition,
   getLogger, setLogger,
   encode, decode,
   CsvExporter, ZipExporter,
