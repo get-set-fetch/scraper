0ced326550b5dc8c20289425edb3724875292e80
Andrei Sabau <andrei@asabau.com>
2021-03-28 19:15:50 +0300

    cheerio acceptance tests cover both compressed and uncompressed content encoding

--- test/acceptance/cheerio.ts
+++ test/acceptance/cheerio.ts
@@ -6,9 +6,7 @@ import * as mysqlConn from '../config/storage/mysql/mysql-conn.json';
 import * as pgConn from '../config/storage/pg/pg-conn.json';
 import CheerioClient from '../../src/domclient/CheerioClient';
 import { ConcurrencyOptions } from '../../src/scraper/ConcurrencyManager';
-import { setLogger } from '../../src';
-
-setLogger({ level: 'debug' });
+import { PluginOpts } from '../../src';
 
 const storage:Storage[] = [
   new KnexStorage(sqliteConn),
@@ -39,13 +37,52 @@ const concurrencyOptions:ConcurrencyOptions[] = [
   },
 ];
 
+const pluginOptions: PluginOpts[][] = [
+  [
+    {
+      name: 'NodeFetchPlugin',
+      headers: {
+        'Accept-Encoding': 'br,gzip,deflate',
+      },
+    },
+  ],
+  [
+    {
+      name: 'NodeFetchPlugin',
+      headers: {
+        'Accept-Encoding': 'identity',
+      },
+    },
+  ],
+];
+
 for (let i = 0; i < storage.length; i += 1) {
   for (let j = 0; j < concurrencyOptions.length; j += 1) {
-    acceptanceSuite(
-      'dom-static-content',
-      storage[i],
-      CheerioClient,
-      concurrencyOptions[j],
-    );
+    /*
+    only when using cheerio
+    for parallel scraping, fetch resources both compressed and uncompressed
+    sequential scraping will fetch using default headers (accepting gzip) like the other acceptance suites
+    */
+    const nodeScrapeWithCustomHeaders = concurrencyOptions[j].proxy && concurrencyOptions[j].proxy.maxRequests > 1;
+
+    if (nodeScrapeWithCustomHeaders) {
+      for (let k = 0; k < pluginOptions.length; k += 1) {
+        acceptanceSuite(
+          'dom-static-content',
+          storage[i],
+          CheerioClient,
+          concurrencyOptions[j],
+          pluginOptions[k],
+        );
+      }
+    }
+    else {
+      acceptanceSuite(
+        'dom-static-content',
+        storage[i],
+        CheerioClient,
+        concurrencyOptions[j],
+      );
+    }
   }
 }
