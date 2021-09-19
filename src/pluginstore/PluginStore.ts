1f366bf7bb2fc2b3aae2148346a74a1300f00ba4
Andrei Sabau <andrei@asabau.com>
2021-09-19 11:37:17 +0300

    relative paths for custom plugins are now correctly resolved when directly invoking PluginStore API

--- src/pluginstore/PluginStore.ts
+++ src/pluginstore/PluginStore.ts
@@ -4,7 +4,7 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
-import { extname, join } from 'path';
+import { extname, join, isAbsolute } from 'path';
 import Plugin, { IPlugin } from '../plugins/Plugin';
 import { getLogger } from '../logger/Logger';
 import { getPackageDir } from '../plugins/file-utils';
@@ -60,11 +60,14 @@ export default class PluginStore {
 
   /**
    * Register a plugin found under the specified path.
-   * @param filepath - file path
+   * @param filepath - absolute or relative to process.cwd filepath
    */
   static async addEntry(filepath: string):Promise<StoreEntry> {
+    const fullFilepath = isAbsolute(filepath) ? filepath : join(process.cwd(), filepath);
+
     try {
-      const pluginModule = await import(filepath);
+      PluginStore.logger.debug('Importing plugin from %s', fullFilepath);
+      const pluginModule = await import(fullFilepath);
       const Cls = pluginModule.default;
       const instance:Plugin = new Cls();
 
@@ -72,11 +75,11 @@ export default class PluginStore {
       let bundle = null;
       if (instance.opts && (instance.opts.domRead || instance.opts.domWrite)) {
         PluginStore.logger.info('Bundling plugin %s', instance.constructor.name);
-        bundle = await PluginStore.buildBundle(filepath);
+        bundle = await PluginStore.buildBundle(fullFilepath);
       }
 
       const storeEntry = {
-        filepath, bundle, Cls,
+        filepath: fullFilepath, bundle, Cls,
       };
       PluginStore.store.set(instance.constructor.name, storeEntry);
       PluginStore.logger.info('Registered plugin %s', instance.constructor.name);
@@ -84,7 +87,7 @@ export default class PluginStore {
       return storeEntry;
     }
     catch (err) {
-      PluginStore.logger.error(err, 'Could not register plugin %s', filepath);
+      PluginStore.logger.error(err, 'Could not register plugin %s', fullFilepath);
       throw (err);
     }
   }
